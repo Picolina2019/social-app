4ae37d20982ac7645676325773ac2da532abdc10
 Katia S <katesakowicz@MacBook-Pro-4.local>
2020-04-30 19:15:39 +0200

    Updates

---
+++ precache-manifest.7fa98418067c4838512e40ee162970b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65ae959c4f56520a3ad3b1685036d4b",
+    "url": "/social1/index.html"
+  },
+  {
+    "revision": "5b005c9b535ff42ef5d1",
+    "url": "/social1/static/css/main.8d9f08af.chunk.css"
+  },
+  {
+    "revision": "5a0642daa568a6979c2e",
+    "url": "/social1/static/js/2.f9d7bb4a.chunk.js"
+  },
+  {
+    "revision": "33c7420501d7b0349de46a3a627e2c05",
+    "url": "/social1/static/js/2.f9d7bb4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b005c9b535ff42ef5d1",
+    "url": "/social1/static/js/main.2ca46368.chunk.js"
+  },
+  {
+    "revision": "ab4bc3605b08045ecf5b",
+    "url": "/social1/static/js/runtime-main.1ed5d01c.js"
+  },
+  {
+    "revision": "63eb7825050aefd0d9a5cbdf9d315ee8",
+    "url": "/social1/static/media/preloader.63eb7825.gif"
+  },
+  {
+    "revision": "cb1043ec3c9101e2a59bb4f04540c61b",
+    "url": "/social1/static/media/profile.cb1043ec.jpg"
+  }
+]);
